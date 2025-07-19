5f730c3de6c1f50d7d7befacf5a707067c8de931
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-19 18:40:30 +0100

    Change font to national park

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<div id="national-park">
+	<slot/>
+</div>
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=National+Park:wght@200..800&display=swap');
+	#national-park {
+		font-family: "National Park", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+	}
+</style>

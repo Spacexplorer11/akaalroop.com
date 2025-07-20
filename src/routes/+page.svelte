00dd23d4c80f7ad2e4071698f22ec11ada29abae
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-20 16:33:53 +0100

    Added typewriter animation, background and text!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to Akaalroop.com</h1>
-<p>This is Akaalroop Singh's website</p>
+<script>
+import Typewriter from 'svelte-typewriter'
+</script>
+<div id="content">
+	<Typewriter mode="cascade" interval="30">
+	<h1><span class="text">Hi! Welcome to Akaalroop.com</span></h1>
+<p><span class="text">This is Akaalroop Singh's website!</span></p>
+	</Typewriter>
+
+</div>
+<style>
+	#content {
+		display: flex;
+		flex-direction: column;
+		padding: 20px;
+		color: rgb(223, 99, 0);
+		min-height: 100vh;
+		text-align: center;
+		font-size: larger;
+	}
+
+	.text {
+		background-color: rgba(0, 0, 0, 0.7); /* black w/ 70% opacity */
+		display: inline-block; /* shrink to fit text width */
+		padding: 0.5em 0.5em;
+		border-radius: 0.5em; /* optional: rounded corners */
+	}
+
+</style>

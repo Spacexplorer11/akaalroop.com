b7e599e4768570f71eb4b903cff5bc54f65b6153
Akaalroop Singh <akaal@akaalroop.com>
2025-07-22 17:15:37 +0100

    Added div with id "navbar" & added styling
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div id="navbar">
+
+</div>
+<style>
+	#navbar {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		padding: 10px 20px;
+		height: 5vmin;
+		background-color: rgba(0, 0, 0, 0.7); /* black with 70% opacity */
+		color: white;
+	}
+</style>

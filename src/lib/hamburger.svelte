f1f3346fe45abb856930a3df0b217838e42423ef
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-22 15:47:35 +0100

    Copied everything from Space Dodge's hamburger menu

--- src/lib/hamburger.svelte
+++ src/lib/hamburger.svelte
@@ -1,3 +1,39 @@
-<div id="menu">
+<script>
+	import {menuOpen} from "$lib/stores/menu.js";
+	import {base} from '$app/paths';
 
+	function toggleMenu() {
+		const menu = document.getElementById("hamburger-menu");
+		const container = document.getElementById("container");
+
+
+		const menuIsActive = menu.classList.contains("active");
+		const containerIsActive = container.classList.contains("active_menu");
+
+		if (menuIsActive && !containerIsActive) {
+			container.classList.toggle("active_menu");
+		} else if (!menuIsActive && containerIsActive) {
+			menu.classList.toggle("active");
+		} else {
+			menu.classList.toggle("active");
+			container.classList.toggle("active_menu");
+		}
+		if (menu.classList.contains("active")) {
+			menuOpen.set(true);
+		} else if (!menu.classList.contains("active")) {
+			menuOpen.set(false);
+			if (container.classList.contains("active_menu")) {
+				container.classList.toggle("active_menu");
+			}
+		}
+	}
+</script>
+
+<button id="hamburger-toggle" on:click={toggleMenu}>☰</button>
+
+<!-- 🍔 Slide-in Menu -->
+<div id="hamburger-menu">
+	<nav>
+		<a href="{base}/">Home</a>
+	</nav>
 </div>

081a6988c917f639cae07626b8e07107ef42b6c4
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-24 19:50:43 +0100

    Add tailwind!
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import "../app.css";
 	import {Navbar} from "$lib";
 </script>
 
@@ -6,8 +7,9 @@
 	<Navbar/>
 	<slot/>
 </div>
+
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=National+Park:wght@200..800&display=swap');
+	@import url("https://fonts.googleapis.com/css2?family=National+Park:wght@200..800&display=swap");
 
 	#national-park {
 		font-family: "National Park", sans-serif;

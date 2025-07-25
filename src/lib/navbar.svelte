4c38b0076f8685f5cb5f7a11885473bc2d88f000
Space <spacexplorer11@proton.me>
2025-07-25 21:14:36 +0100

    Update src/lib/navbar.svelte
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Space <akaal@akaalroop.com>

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="items-center ml-6 @container relative flex w-full flex-row justify-between bg-black/70 p-10 text-white">
-	<nav class="gap-20rem flex flex-row">
+	<nav class="gap-20 flex flex-row">
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/">Home</a>
 		<!--    <a href="/projects">Projects</a> -->
 		<!--	<a href="/about">About</a> -->

c4c47a1e397a5f736dea309f91ca90d83ef2e9ae
Akaalroop Singh <akaal@akaalroop.com>
2025-07-26 11:34:46 +0100

    Ran prettier
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="@container relative flex w-full flex-row items-center justify-between bg-black/70 p-10 text-white ml-0">
+<div class="@container relative ml-0 flex w-full flex-row items-center justify-between bg-black/70 p-10 text-white">
 	<nav class="flex flex-row gap-20">
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/">Home</a>
 		<!--    <a href="/projects">Projects</a> -->

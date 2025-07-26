defd23b683a00b68a0025a36b4ee55c3cb2e675e
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-26 12:56:13 +0100

    Changed typewriter mode to cascade
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,7 +3,7 @@
 	import Typewriter from "svelte-typewriter";
 </script>
 <div class="@container mt-10 mb-10 flex min-h-screen flex-col p-5 text-center text-orange-500">
-	<Typewriter>
+	<Typewriter mode="cascade">
 <h1><span class="inline-block p-[0.5em] bg-black/70 rounded-[0.5em] overflow-hidden whitespace-normal max-w-fit">Projects!</span>
 </h1>
 		<p><span class="inline-block p-[0.5em] bg-black/70 rounded-[0.5em] overflow-hidden whitespace-normal max-w-fit">You've probably already seen some of my projects, right?</span>

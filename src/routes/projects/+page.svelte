0a8369765a09f6c00a6d3d1f9a6468b1758afac0
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-26 12:53:18 +0100

    Fixed project's title by adding content container
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,10 @@
 <script>
 
+	import Typewriter from "svelte-typewriter";
 </script>
+<div class="@container mt-10 mb-10 flex min-h-screen flex-col p-5 text-center text-orange-500">
+	<Typewriter>
 <h1><span class="inline-block p-[0.5em] bg-black/70 rounded-[0.5em] overflow-hidden whitespace-normal max-w-fit">Projects!</span>
 </h1>
+	</Typewriter>
+</div>

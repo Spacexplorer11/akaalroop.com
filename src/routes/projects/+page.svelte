d1c91e9df62778f79a71e55fcef5fd1a04b58e9a
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-26 12:56:01 +0100

    Added more text
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,5 +6,7 @@
 	<Typewriter>
 <h1><span class="inline-block p-[0.5em] bg-black/70 rounded-[0.5em] overflow-hidden whitespace-normal max-w-fit">Projects!</span>
 </h1>
+		<p><span class="inline-block p-[0.5em] bg-black/70 rounded-[0.5em] overflow-hidden whitespace-normal max-w-fit">You've probably already seen some of my projects, right?</span>
+		</p>
 	</Typewriter>
 </div>

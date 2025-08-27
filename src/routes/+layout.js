70629e1a25f256c290a76734293585f5949c80b5
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-27 11:14:44 +0100

    Hopefully fix localStorage is not defined error when deploying
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,12 +3,11 @@ import { fetchProjects, loadProjectsClicked } from "$lib/projects.svelte.js";
 export const load = async () => {
 	try {
 		await fetchProjects();
-		if (typeof window !== undefined && localStorage !== undefined) {
+		if (window !== undefined && typeof localStorage !== undefined) {
 			await loadProjectsClicked();
 		}
 	} catch (error) {
 		console.error("Failed to load projects in layout:", error);
-		// Continue loading the page even if projects fail to fetch
 	}
 	return {};
 };

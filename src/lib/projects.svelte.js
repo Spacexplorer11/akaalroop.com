202e48e6e5b5f11ea8123759b674a83392d3bc05
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-27 18:38:14 +0100

    Reorder repos alphabetically
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/lib/projects.svelte.js
+++ src/lib/projects.svelte.js
@@ -32,17 +32,17 @@ export async function saveProjectsClicked() {
 
 export const projectRepos = [
 	"akaalroop.com",
-	"meow_meals",
-	"space_dodge",
-	"word_ban",
-	"trafalgar-to-trenches",
 	"cloudcat",
-	"supercalculator",
-	"lets-do-it",
-	"rust-is-the-best",
+	"colourin",
 	"justcount",
+	"lets-do-it",
+	"meow_meals",
+	"rust-is-the-best",
+	"space_dodge",
+	"supercalculator",
+	"trafalgar-to-trenches",
 	"winter-of-making-minecraft-mod",
-	"colourin"
+	"word_ban"
 ];
 export let projects = [];
 

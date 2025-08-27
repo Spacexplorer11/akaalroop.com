b1340dafca5ffe62c7fdc253433ffec97158094f
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-27 10:44:22 +0100

    Added expiry to the projectsClicked so if its older than 5minutes it will be removed, to make sure stale data is not used
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/lib/projects.svelte.js
+++ src/lib/projects.svelte.js
@@ -1,13 +1,31 @@
 export let projectsClicked = $state(new Set());
 
 export async function loadProjectsClicked() {
-	const saved = localStorage.getItem("projectsClicked");
-	if (saved) JSON.parse(saved).forEach((item) => projectsClicked.add(item));
+	const savedStr = localStorage.getItem("projectsClicked");
+	if (!saved) return;
+
+	try {
+		const savedObj = JSON.parse(savedStr);
+		const now = Date.now();
+		if (savedObj.expiry && now < savedObj.expiry) {
+			savedObj.value.forEach((item) => projectsClicked.add(item));
+		} else {
+			localStorage.removeItem("projectsClicked");
+		}
+	} catch {
+		localStorage.removeItem("projectsClicked");
+	}
 }
 
 export async function saveProjectsClicked() {
-	localStorage.setItem("projectsClicked", JSON.stringify(Array.from(projectsClicked)));
+	const expiry = Date.now() + 300_000;
+	const obj = {
+		value: Array.from(projectsClicked),
+		expiry
+	};
+	localStorage.setItem("projectsClicked", JSON.stringify(obj));
 }
+
 export const projectRepos = [
 	"akaalroop.com",
 	"meow_meals",

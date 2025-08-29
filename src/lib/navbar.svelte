81f1309b1d3b2ecf7504ee7d3b2dfd83a4e98eeb
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-29 23:34:36 +0100

    Add sponsor button
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -2,12 +2,16 @@
 </script>
 
 <div class="@container relative ml-0 flex w-full flex-row items-center justify-between bg-black/70 p-10 text-white">
-	<nav class="flex flex-row space-x-8 md:space-x-16 lg:space-x-24">
+	<nav class="flex flex-row space-x-8 md:space-x-16 lg:space-x-24 w-full">
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/">Home</a>
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline"
 		   href="/projects">Projects</a>
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/gallery">Gallery</a>
 		<!--	<a href="/about">About</a> -->
 		<!--	<a href="/contact">Contact</a> -->
+		<div id="sponsor-button" class="ml-auto self-start">
+			<iframe src="https://github.com/sponsors/Spacexplorer11/button" title="Sponsor Spacexplorer11" height="32"
+			        width="114" style="border: 0; border-radius: 6px;"></iframe>
+		</div>
 	</nav>
 </div>

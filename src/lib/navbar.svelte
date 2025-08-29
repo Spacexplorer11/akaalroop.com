a729b5091f3761fa2bc0c6e67572c3e76c3dab0f
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-30 00:05:38 +0100

    Added buy me a coffee button
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -2,16 +2,34 @@
 </script>
 
 <div class="@container relative ml-0 flex w-full flex-row items-center justify-between bg-black/70 p-10 text-white">
-	<nav class="flex flex-row space-x-8 md:space-x-16 lg:space-x-24 w-full">
+	<nav class="flex w-full flex-row items-center space-x-8 md:space-x-16 lg:space-x-24">
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/">Home</a>
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline"
 		   href="/projects">Projects</a>
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/gallery">Gallery</a>
 		<!--	<a href="/about">About</a> -->
 		<!--	<a href="/contact">Contact</a> -->
-		<div id="sponsor-button" class="ml-auto self-start">
-			<iframe src="https://github.com/sponsors/Spacexplorer11/button" title="Sponsor Spacexplorer11" height="32"
-			        width="114" style="border: 0; border-radius: 6px;"></iframe>
+		<div id="sponsor-buttons" class="ml-auto flex flex-row items-center space-x-2">
+			<iframe
+					src="https://github.com/sponsors/Spacexplorer11/button"
+					title="Sponsor Spacexplorer11"
+					height="32"
+					width="114"
+					style="border: 0; border-radius: 6px;"
+			></iframe>
+			<script
+					type="text/javascript"
+					src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+					data-name="bmc-button"
+					data-slug="spacexplorer11"
+					data-color="#FFDD00"
+					data-emoji="☕"
+					data-font="Cookie"
+					data-text="Buy me a coffee"
+					data-outline-color="#000000"
+					data-font-color="#000000"
+					data-coffee-color="#ffffff"
+			></script>
 		</div>
 	</nav>
 </div>

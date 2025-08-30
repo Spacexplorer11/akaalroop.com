9e7ac8a24eb613f0f30c68f6b471948105eecdec
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-30 20:00:20 +0100

    Made the sponsor & BMC button mobile-responsive! Navbar looks much better across screen sizes now!
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,11 +1,11 @@
-<div class="@container relative ml-0 flex w-full flex-row items-center justify-between bg-black/70 p-10 text-white">
-	<nav class="flex w-full flex-row items-center space-x-8 md:space-x-16 lg:space-x-24">
+<div class="@container relative ml-0 flex w-full items-center justify-between bg-black/70 p-10 text-white md:flex-row">
+	<nav class="flex w-full flex-wrap items-center space-x-8 gap-y-4 md:space-x-16 lg:space-x-24">
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/">Home</a>
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/projects">Projects</a>
 		<a class="text-2xl font-medium text-orange-500 hover:text-orange-600 hover:overline" href="/gallery">Gallery</a>
 		<!--	<a href="/about">About</a> -->
 		<!--	<a href="/contact">Contact</a> -->
-		<div id="sponsor-buttons" class="ml-auto flex flex-row items-center space-x-2">
+		<div id="sponsor-buttons" class="flex flex-shrink-0 flex-row items-center space-x-2 lg:ml-auto">
 			<iframe
 				src="https://github.com/sponsors/Spacexplorer11/button"
 				title="Sponsor Spacexplorer11"

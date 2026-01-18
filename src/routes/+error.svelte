61df44c05463e8135edff10906aed023ec1493ce
Akaalroop Singh <akaal@akaalroop.com>
2026-01-18 13:09:08 +0000

    Run prettier on +error.svelte
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -31,5 +31,5 @@
 			>
 		</p>
 	{/if}
-	<img src="https://http.cat/{page.status}.jpg" alt="HTTP {page.status} error cat image" class="mt-8 mx-auto" />
+	<img src="https://http.cat/{page.status}.jpg" alt="HTTP {page.status} error cat image" class="mx-auto mt-8" />
 </div>

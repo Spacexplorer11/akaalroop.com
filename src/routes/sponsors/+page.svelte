8a727625cc78d749aa4921e82e5073e84f2723a0
Akaalroop Singh <akaal@akaalroop.com>
2026-01-20 18:49:44 +0000

    Finish the sponsors page
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

---
+++ src/routes/sponsors/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Typewriter from "svelte-typewriter";
+</script>
+
+<div class="@container mx-auto px-4 py-8 text-center">
+	<Typewriter mode="cascade" interval="20">
+		<h1 class="text-orange-500"><span class="text-bg">My Amazing Sponsors</span></h1>
+		<h2 class="text-orange-500">
+			<span class="text-bg"
+				>First of all, I'd like to thank my first sponsors, my parents for everything they have ever done for me! ❤️
+			</span>
+		</h2>
+	</Typewriter>
+	<Typewriter delay="3800" mode="cascade" interval="10">
+		<!-- <div class="text-orange-500  mt-5"><h1 class="text-bg">Platinum (£500-*)</h1></div>
+		<div class="text-orange-500  mt-5"><h1 class="text-bg">Gold (£200-£500)</h1></div>
+		<div class="text-orange-500  mt-5"><h1 class="text-bg">Silver (£50-£200)</h1></div> -->
+		<div class="mt-5 text-orange-500"><h1 class="text-bg">Bronze (£0-£50)</h1></div>
+	</Typewriter>
+</div>

5c0dfc85ac50b03b81be4f88a8c0c99f2c64a175
Akaalroop Singh <akaal@akaalroop.com>
2026-01-24 13:12:39 +0000

    Moved the font in $lib for optimisation!
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,12 +8,31 @@
 
 	onMount(async () => {
 		try {
-			await loadProjectsClicked(localStorage);
+			loadProjectsClicked(localStorage);
 		} catch (error) {
 			console.error("Failed to load projects in layout:", error);
 		}
 	});
 </script>
 
+<div id="national-park">
 <Navbar {data} />
 {@render children()}
+</div>
+
+<style>
+	@font-face {
+		font-family: "National Park";
+		font-style: normal;
+		font-weight: 600;
+		src: url("$lib/fonts/NationalPark-ExtraBold.woff2") format("woff2");
+		font-display: swap;
+	}
+
+	#national-park {
+		font-family: "National Park", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+	}
+</style>

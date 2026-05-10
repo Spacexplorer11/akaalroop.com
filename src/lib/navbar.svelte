e0ed14fb496e5fe8f93d1819158e8b275d950bf0
Space <akaal@akaalroop.com>
2026-05-10 17:58:30 +0100

    Remove lazy loading from image in navbar
    Removed lazy loading from Buy Me A Coffee button image.
    
    Signed-off-by: Space <akaal@akaalroop.com>

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -33,7 +33,6 @@
 				<img
 					src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
 					alt="Buy Me A Coffee"
-					loading="lazy"
 					width="217"
 					height="60"
 					style="height: 60px !important; width: 217px !important;"
